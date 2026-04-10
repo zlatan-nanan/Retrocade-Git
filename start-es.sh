873e5956508964cac2b784bbe22ffeef0973a7e9
zlatan-nanan <zlatan@nananacademy.com>
2026-04-10 12:25:52 -0700

    Disabled screensaver and power management

--- start-es.sh
+++ start-es.sh
@@ -3,5 +3,11 @@
 source /home/nanan/.profile 2>/dev/null
 source /home/nanan/.bashrc 2>/dev/null
 sleep 5
+
+# Disable screensaver and display power management
+xset s off
+xset -dpms
+xset s noblank
+
 # Use script to allocate a pseudo-TTY so runcommand.sh can open /dev/tty
 exec script -q -c /usr/bin/emulationstation /dev/null
